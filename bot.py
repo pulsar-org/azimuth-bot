58fe2532825f7689548417d205c6cf06f433e0ed
stationswift001 <36418561+stationswift001@users.noreply.github.com>
2023-04-20 22:41:47 -0500

    bot actually works
    to an extent

--- bot.py
+++ bot.py
@@ -1,48 +1,28 @@
 import discord
-from discord.ext import commands, tasks
-import asyncio
-from discord_slash import SlashCommand, cog_ext, SlashContext
-from discord_slash.utils.manage_commands import create_option, create_choice
 
-intents = discord.Intents.all()
-client = commands.Bot(command_prefix=['!'], intents=intents)
-slash = SlashCommand(client, sync_commands=True, sync_on_cog_reload=True)
+bot = discord.Bot()
 
 
-@client.event
-async def on_ready():
-    await client.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.listening, name=f"INFINIX#7276 (My Dev/Creator)"))
+@bot.slash_command(name = "test", description = "Latest testing command")
+async def hello(ctx):
+    embed = discord.Embed(
+        title="My Amazing Embed",
+        description="Embeds are super easy, barely an inconvenience.",
+        color=discord.Colour.blurple(),  # Pycord provides a class with default colors you can choose from
+    )
+    embed.add_field(name="A Normal Field",
+                    value="A really nice field with some information. **The description as well as the fields support markdown!**")
 
-    print("Bot Made by INFINIX#7276")
+    embed.add_field(name="Inline Field 1", value="Inline Field 1", inline=True)
+    embed.add_field(name="Inline Field 2", value="Inline Field 2", inline=True)
+    embed.add_field(name="Inline Field 3", value="Inline Field 3", inline=True)
 
+    embed.set_footer(text="Footer! No markdown here.")  # footers can have icons too
+    embed.set_author(name="Pycord Team", icon_url="https://example.com/link-to-my-image.png")
+    embed.set_thumbnail(url="https://example.com/link-to-my-thumbnail.png")
+    embed.set_image(url="https://example.com/link-to-my-banner.png")
 
-@slash.slash(name="slashtest",
-             description="A test command (You can change it)",
-             options=[
-                 create_option(
-                     name="opt",
-                     description="The option",
-                     option_type=3,
-                     # check out the docs (link is provided in readme.md file) to know more about different types of options
-                     required=False,
-                     choices=[
-                         create_choice(
-                             name="FirstChoise",
-                             value="Happy"
-                         ),
-                         create_choice(
-                             name="SecondChoice",
-                             value="Sad :("
-                         )
-                     ]
-                 )
-             ])
-async def test(ctx, optone: str):
-    await ctx.send(content=f"Okay! I'm setting your current mood to {opt} :p")
-    await ctx.send(
-        content="Contact `INFINIX#7276` to know more!\n\nJoin our discord at https://www.discord.gg/DNvFcrhdzr")
+    await ctx.respond("Hello! Here's a cool embed.", embed=embed)  # Send the embed with some text
 
 
-token = bot
-client.run(token)
+bot.run("MTA5ODgwODE0MDQzNjM1NzIzMg.GRMUha.t1YMF2IC1_N-bWuAkfLkEqHXjEnHTpV4VB4DsY")
